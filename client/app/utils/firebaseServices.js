51b0c973f1882b92b2a85fc3331eb6b834861115
= <toloverusss2@gmail.com>
2025-11-07 10:05:55 -0500

    Finished implementing chat history restoration

--- client/app/utils/firebaseServices.js
+++ client/app/utils/firebaseServices.js
@@ -2,14 +2,14 @@ import axios from "axios"
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
-export const createUser = async (userId) => {
+export const createUser = (userId) => {
     console.log("backend url:", BACKEND_URL)
     const payload = {
         "userId": String(userId)
     }
     return axios.post(`${BACKEND_URL}/users`, payload)
         .then(res => {
-            console.log("res", res)
+            console.log("res:", res.data)
             return res.data
         })
 }
